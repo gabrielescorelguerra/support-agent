a0998ae98aa2e09331d54649f095560fcef9d5d8
Gabriel Guerra <gabrielcake9@gmail.com>
2026-09-09 02:46:45 -0300

    altera settings

--- src/ia_suporte/config.py
+++ src/ia_suporte/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     gemini_api_key: str
     telegram_bot_token: str
+    webhook_url: str
+    telegram_webhook_secret: str
     # support_api_url: str
     # support_api_token: str
     # environment: str = "development"
