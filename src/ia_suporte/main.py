967621303ff2f9a146c230b200faa8d1613c0cf5
Gabriel Guerra <gabrielcake9@gmail.com>
2026-09-09 02:44:12 -0300

    corrige importação e comenta main

--- src/ia_suporte/main.py
+++ src/ia_suporte/main.py
@@ -1,27 +1,27 @@
-# # EXEMPLO
-# triage_llm = GeminiClient()
-# response_llm = GroqClient()
+# # # EXEMPLO
+# # triage_llm = GeminiClient()
+# # response_llm = GroqClient()
 
-# triage_agent = TriageAgent(
-#     triage_llm=triage_llm,
-#     response_llm=response_llm
+# # triage_agent = TriageAgent(
+# #     triage_llm=triage_llm,
+# #     response_llm=response_llm
+# # )
+
+# # from .base import LLM
+
+# from ia_suporte.agents.base import WebhookData
+# from ia_suporte.agents.triage.agent import TriageAgent
+# from ia_suporte.llm.gemini import GeminiLLM
+
+# gemini_2_5_flash = GeminiLLM("gemini-3.1-flash-lite")
+# gemini_3_1_flash = GeminiLLM("gemini-3.1-flash")
+
+# data = WebhookData(
+#     chat_id=123, name="Roberto", email="a@gmail.com", messages=["oi", "ok"]
 # )
 
-# from .base import LLM
+# triage_agent = TriageAgent(llm=gemini_2_5_flash, data=data)
 
-from ia_suporte.agents.base import WebhookData
-from ia_suporte.agents.triage.agent import TriageAgent
-from ia_suporte.llm.gemini import GeminiLLM
-
-gemini_2_5_flash = GeminiLLM("gemini-3.1-flash-lite")
-gemini_3_1_flash = GeminiLLM("gemini-3.1-flash")
-
-data = WebhookData(
-    chat_id=123, name="Roberto", email="a@gmail.com", messages=["oi", "ok"]
-)
-
-triage_agent = TriageAgent(llm=gemini_2_5_flash, data=data)
-
-# if for pra triagem
-response = triage_agent.run()
-print(response)
+# # if for pra triagem
+# response = triage_agent.run()
+# print(response)
